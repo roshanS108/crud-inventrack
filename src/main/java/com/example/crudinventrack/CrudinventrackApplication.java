20520db7a454802fb683c6ccd29c26b3e726baf0
roshankumar811 <roshanshoti100@gmail.com>
2023-03-17 16:56:08 -0400

    initial commit

---
+++ src/main/java/com/example/crudinventrack/CrudinventrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudinventrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudinventrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudinventrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudinventrack/CrudinventrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudinventrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudinventrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
