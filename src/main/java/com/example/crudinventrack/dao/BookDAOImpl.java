349e6832c6115dff1220f7db4b70cfeff4e4012f
roshankumar811 <roshanshoti100@gmail.com>
2023-03-23 22:33:07 -0400

    Injected the Entity Manager through constructor

--- src/main/java/com/example/crudinventrack/dao/BookDAOImpl.java
+++ src/main/java/com/example/crudinventrack/dao/BookDAOImpl.java
@@ -1,9 +1,10 @@
 package com.example.crudinventrack.dao;
 import com.example.crudinventrack.entity.Book;
+import com.example.crudinventrack.entity.Customer;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
 @Repository
 public class BookDAOImpl implements BookDAO{
 
@@ -11,14 +12,22 @@ public class BookDAOImpl implements BookDAO{
     private EntityManager entityManager;
 
     //inject entity manager using constructor injection
+    @Autowired
     public BookDAOImpl(EntityManager entityManager){
         this.entityManager = entityManager;
     }
-
     //implement save method
     @Override
     @Transactional
     public void saveBook(Book theBook) {
-        entityManager.persist(theBook);
+        Book manageBook = entityManager.merge(theBook);
+        entityManager.persist(manageBook);
+
     }
+
+    @Override
+    public Book readBookData(Integer id) {
+        return entityManager.find(Book.class, id);
+    }
+
 }
