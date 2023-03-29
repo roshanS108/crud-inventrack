9b2c43424dd482004390ff428f93fec5c17e2271
roshan kumar <60356811+roshanKumar811@users.noreply.github.com>
2023-03-29 14:50:38 -0400

    Update Customer.java

--- src/main/java/com/example/crudinventrack/entity/Customer.java
+++ src/main/java/com/example/crudinventrack/entity/Customer.java
@@ -8,15 +8,46 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "customer_Id")
-    private int customer_Id;
+    private int customerId;
 
     @Column(name = "first_name")
     private String firstName;
 
     @Column(name = "last_name")
-    private String last_name;
+    private String lastName;
 
     @Column(name = "email")
     private String email;
-
+    
+    public Customer(){
+        
+    }
+    public Customer(String firstName, String lastName, String email){
+        this.firstName = firstName; 
+        this.lastName = lastName; 
+        this.email = email; 
+    }
+    public String getFirstName(){
+        return this.firstName;    
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName; 
+    }
+    
+    public String getLastName(){
+        return this.lastName;    
+    }
+    public void setLastName(String lastName){
+        this.firstName = firstName; 
+    }
+    
+    public String getEmail(){
+        return this.email; 
+    }
+    public void setEmail(String email){
+        this.email = email;    
+    }
+    
+    
+    
 }
